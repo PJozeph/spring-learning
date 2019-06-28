ecdf90c04e32be6207250f4007ce3635ba24d47a
PJoseph <pallagijoe@gmail.com>
2019-06-28 08:20:58 +0200

    bootstrap

---
+++ spring-security-demo-04-bootstrap-login-form/src/main/java/home/joci/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package home.joci.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
